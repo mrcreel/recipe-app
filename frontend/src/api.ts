3c7fd0b2d31d2eeac8a1ed9c2efcfaa43b82fa00
Michael R Creel <michaelrcreel@gmail.com>
2023-12-31 08:52:17 -0600

    Create frontend call to remoite api

---
+++ frontend/src/api.ts
@@ -0,0 +1,11 @@
+export const searchRecipes = async (searchTerm: string, page: number) => {
+  const baseUrl = new URL("http://localhost:5000/api/recipes/search")
+  baseUrl.searchParams.append("searchTerm", searchTerm)
+  baseUrl.searchParams.append("page", page.toString())
+
+  const response = await fetch(baseUrl)
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status code: ${response.status}`)
+  }
+  return response.json()
+}
