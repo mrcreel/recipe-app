1ccf6fe2ecf7953cf83ba13d94f9e343004544d2
Michael R Creel <michaelrcreel@gmail.com>
2023-12-31 08:28:31 -0600

    Created recipe search backen point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
 import express, { application } from "express"
 import cors from "cors"
 
-import * as reccipeApi from "./recipe-api"
+import * as recipeApi from "./recipe-api"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/api/recipe/search", async (req, res) => {
-  res.json({ message: "Success" })
+app.get("/api/recipes/search", async (req, res) => {
+  const searchTerm = req.query.searchTerm as string
+  const page = parseInt(req.query.page as string)
+  const results = await recipeApi.searchRecipes(searchTerm, page)
+
+  return res.json(results)
 })
 
 app.listen(5000, () => {
