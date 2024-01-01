8d0ff1b3df5e82c2b8cd2e0d2a15b2d25dcbecd4
Michael R Creel <michaelrcreel@gmail.com>
2024-01-01 07:03:00 -0600

    Add favorite icon to RecipeCard

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineHeart } from "react-icons/ai"
 import { Recipe } from "../types"
 
 interface Props {
@@ -10,6 +11,9 @@ const RecipeCard = ({ recipe, onClick }: Props) => {
     <div className='recipeCard' onClick={onClick}>
       <img src={recipe.image} alt={`Image of ${recipe.title}`} />
       <div className='recipeCardTitle'>
+        <span>
+          <AiOutlineHeart size={25} />
+        </span>
         <h3>{recipe.title}</h3>
       </div>
     </div>
